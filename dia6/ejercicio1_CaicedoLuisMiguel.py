63145f3c2de0e07ab3cb5b1eaf72978637e7fbe3
luisMiguel <luismiguelcaicedobermon@gmail.com>
2024-04-16 22:34:31 -0500

    commit12

--- dia6/ejercicio1_CaicedoLuisMiguel.py
+++ dia6/ejercicio1_CaicedoLuisMiguel.py
@@ -1,17 +1,17 @@
 ver=True
 while ver == True:
     numlis=int(input("cuantos numeros tiene tu lista\n"))
-    if numlis<300 and numlis>0:
-        ver = False
-    if numlis>=300 or numlis<1:
+    if numlis<300 and numlis>0:#mientras que la cantidad de numeros sea menor a 300 y mayor a 0
+        ver = False#ver será falso y seguirá el proceso
+    if numlis>=300 or numlis<1:#si no es así se repetirá la pregunta
         print("la lista no puede tener mas de 300 numeros o menos de uno")
-num= []
-for i in range(numlis):
-    numero=int(input())
-    num.append(numero)
+num= []#es donde se almacenará la lista
+for i in range(numlis):#va desde 0 hasta la cantidad de numeros en la lista
+    numero=int(input())#se piden los numeros
+    num.append(numero)#y se van agregando a la lista
 
-num.sort()
-num=set(num)
+num.sort()#se ordenan los numeros de la lista de menor a mayor
+num=set(num)#esto elimina los numeros repetidos
 
-print(num)
+print(num)#se imprimen los numeros
 #DESARROLLADO POR: Caicedo Bermon Luis Miguel c.c: 1090381345
--- dia6/ejercicio2_caicedoLuisMiguel.py
+++ dia6/ejercicio2_caicedoLuisMiguel.py
@@ -1,15 +1,18 @@
 lis= input()
-lista=[int(i) for i in lis.split()]
-print(lista)
+lista=[int(i) for i in lis.split()]#se pide una lista de numeros separando los numeros por espacios
+print(lista)#se muestra la lista al usuario aunque es algo innesesario
 
-suma=int(input())
+suma=int(input())#se le pide al usuario la suma
 
-
-for i in range(len(lista)):
-    z=i+1
-    for z in range(z,len(lista)):
-        su=lista[i]+lista[z]
-        if su==suma:
-            print(i,z) 
+a=0#es un contador
+for i in range(len(lista)):#comienza desde el primer numero de la lista hasta el ultimo
+    z=i+1#es para que el siguiente numero a analizar no sea el mismo que el primero
+    for z in range(z,len(lista)):#entonces empieza desde el segundo
+        su=lista[i]+lista[z]#se suma el primer numero con el que le sigue para comprobar si da el resultado que se quiere
+        if a==0:#si el contador es igual a 0 se va a realizar lo siguiente
+            if su==suma:#si el resultado de la suma anterior es igual a el numero que da el usuario
+                print(i,z)#se van a imprimir el numero de puesto en el que estaban los numeros
+                a=a+1#se le suma 1 al contador por si hay otro resultado posible ya no lo imprima
+            
 
 #DESARROLLADO POR: Caicedo Bermon Luis Miguel c.c: 1090381345
