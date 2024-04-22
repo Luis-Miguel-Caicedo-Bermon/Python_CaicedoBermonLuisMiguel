49f58395fc9105a5781d63a89339d72d9b8692a2
JaimeBarreraS <jaenba10@gmail.com>
2024-04-22 12:55:21 -0500

    commit15

---
+++ dia10/ejercicio.py
@@ -0,0 +1,10 @@
+lis=input()
+lista=[int(i) for i in lis.split()]
+print(lista)
+
+num=int(input())
+for i in range(len(lista)):
+    if num==lista[i]:
+        print(i)
+    if num>lista[i] and num<lista[i+1]:
+        print(i+1)
