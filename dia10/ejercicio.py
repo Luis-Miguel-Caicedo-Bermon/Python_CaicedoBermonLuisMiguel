8fdfe6c1c7908154f6581e39bed3c2603f4d92ac
luisMiguel <luismiguelcaicedobermon@gmail.com>
2024-04-22 16:56:49 -0500

    commit15

--- dia10/ejercicio.py
+++ dia10/ejercicio.py
@@ -1,11 +1,13 @@
-lis=input()
-lista=[int(i) for i in lis.split()]
-print(lista)
+lis=input()#se piden los numeros separados por espacios
+lista=[int(i) for i in lis.split()]#se transforma en una lista
 
+c=0#un contador
 num=int(input())
-for i in range(len(lista)):
-    if num==lista[i]:
-        print(i)
-    if num>lista[i] and num<lista[i+1]:
-        print(i+1)
-        
+for i in range(len(lista)):#va hasta el ultimo numero de la lista
+    if num==lista[i]:#si e el numero es igual al numero en la posicion i
+        print(i)#se imprime la posicion
+    if num not in lista:#si el numero no esta en la lista
+        if num>lista[i]:#si el numero es mayor al numero en esa posicion
+            c=c+1#se suma 1 a contador que va contando las casillas
+if num not in lista:
+    print(c)#se imprime el valor del contador solo si el numero no está enn la lista
---
+++ dia8/ejercicio2_CaicedoLuisMiguel.py
@@ -0,0 +1,3 @@
+fruits =  [("banana", 0.45, 6), ("jackfruit", 4.55, 2), ("qiwi", 0.15, 23)]
+fruits[0]=fruits[1]
+print(fruits)
